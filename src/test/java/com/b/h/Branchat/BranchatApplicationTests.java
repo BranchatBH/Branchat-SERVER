fa70ea5e351f367d3a48a2db44db30418db5c71d
lezgititt <bsj6734@gmail.com>
2025-08-05 20:46:11 +0900

    gitignore 파일 추가

---
+++ src/test/java/com/b/h/Branchat/BranchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b.h.Branchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
