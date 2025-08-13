0fde3f9a00f388b71eb923001cb54b0473d77bfd
lezgititt <bsj6734@gmail.com>
2025-08-13 22:40:55 +0900

    :zap: CICD..찐막 제발

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -9,8 +9,8 @@ docker logout
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
-# 시스템이 자동으로 credsStore를 추가하더라도, 이 명령어가 일시적으로 무시시킵니다.
 export DOCKER_CREDENTIALS_STORE=""
 
-# --pull 옵션으로 pull과 up을 한번에 실행
-docker compose --env-file ./.env up -d --pull
+# ### 👇 여기가 최종 수정 지점입니다. ###
+# --pull 뒤에 'always'를 추가하여 항상 새 이미지를 받도록 합니다.
+docker compose --env-file ./.env up -d --pull always
