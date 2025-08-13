dfa338cc0a78430a6744cb5d3a092de80e4f320a
lezgititt <bsj6734@gmail.com>
2025-08-13 23:13:42 +0900

    :zap: CICD..찐막 제발

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -9,7 +9,5 @@ docker logout
 
 echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
-# export DOCKER_CREDENTIALS_STORE=""  <-- 이제 필요 없으므로 삭제!
-# --env-file ./.env                 <-- 이것도 보통 자동 인식되므로 삭제해도 무방
-
-docker compose up -d --pull always
+export DOCKER_CREDENTIALS_STORE=""
+docker compose --env-file ./.env up -d --pull always
