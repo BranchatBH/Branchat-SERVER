65e30e76d574b5a304c0e2b7f77c5fc7a4837fb3
lezgititt <bsj6734@gmail.com>
2025-08-13 23:03:56 +0900

    :zap: CICD..찐막 제발

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# CodeDeploy가 파일을 복사해준 위치로 이동
 cd /home/ubuntu/app
 
-# 실행 중인 컨테이너가 있으면 내리기
-if [ "$(docker ps -q -f name=branchat-api)" ]; then
-    docker compose -f docker-compose.yml down
+# docker-compose.yml 파일이 존재하는 경우에만 down 명령어를 실행
+if [ -f docker-compose.yml ]; then
+    docker compose down
 fi
